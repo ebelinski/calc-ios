eea52bddcde6277c1a0edfb653ff6fffada95abd
Eugene Belinski <ebelinski@protonmail.com>
2018-03-24 22:55:45 -0500

    removed storyboard

--- calc/AppDelegate.swift
+++ calc/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  calc
-//
-//  Created by Eugeniu Belinski on 3/24/18.
-//  Copyright © 2018 Eugeniu Belinski. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -15,7 +7,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
+
     return true
   }
 
@@ -43,4 +38,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-

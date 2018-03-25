e99f79488ddb0dea70243aa5b9d386ddae5163f9
Eugene Belinski <ebelinski@protonmail.com>
2018-03-24 23:24:38 -0500

    added add button

--- calc/CalculatorViewController.swift
+++ calc/CalculatorViewController.swift
@@ -2,4 +2,27 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+  let addButton: UIButton = {
+    let addButton = UIButton()
+    addButton.translatesAutoresizingMaskIntoConstraints = false
+    addButton.setTitle("+", for: .normal)
+    addButton.setTitleColor(.blue, for: .normal)
+    addButton.addTarget(self,
+                        action: #selector(CalculatorViewController.didPressAddButton),
+                        for: .touchUpInside)
+    return addButton
+  }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.addSubview(addButton)
+    addButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    addButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
+
+  @objc func didPressAddButton() {
+    print("Pressed add button")
+  }
+
 }

3381ad6f056393e19901dbdd409106f90c07c931
Eugene Belinski <ebelinski@protonmail.com>
2018-03-25 07:48:48 -0500

    added gray background to CalculatorButton

--- calc/CalculatorButton.swift
+++ calc/CalculatorButton.swift
@@ -5,6 +5,7 @@ class CalculatorButton: UIButton {
   override func didMoveToSuperview() {
     super.didMoveToSuperview()
     translatesAutoresizingMaskIntoConstraints = false
+    backgroundColor = Color.grayBackground
   }
 
 }
---
+++ calc/Color.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+enum Color {
+  static let grayBackground = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1)
+}

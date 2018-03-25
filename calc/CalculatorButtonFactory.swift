e12df43be90f503c03d89d2d475e02012345f53b
Eugene Belinski <ebelinski@protonmail.com>
2018-03-25 12:49:07 -0500

    neater button action array

--- calc/CalculatorButtonFactory.swift
+++ calc/CalculatorButtonFactory.swift
@@ -3,24 +3,14 @@ import Foundation
 struct CalculatorButtonFactory {
 
   static func buttons() -> [CalculatorButton] {
-    return [
-      CalculatorButton(withAction: .number(9)),
-      CalculatorButton(withAction: .number(8)),
-      CalculatorButton(withAction: .number(7)),
-      CalculatorButton(withAction: .divide),
-      CalculatorButton(withAction: .number(6)),
-      CalculatorButton(withAction: .number(5)),
-      CalculatorButton(withAction: .number(4)),
-      CalculatorButton(withAction: .multiply),
-      CalculatorButton(withAction: .number(3)),
-      CalculatorButton(withAction: .number(2)),
-      CalculatorButton(withAction: .number(1)),
-      CalculatorButton(withAction: .subtract),
-      CalculatorButton(withAction: .number(0)),
-      CalculatorButton(withAction: .addDecimal),
-      CalculatorButton(withAction: .delete),
-      CalculatorButton(withAction: .add),
+    let actions: [CalculatorButton.Action] = [
+      .number(9), .number(8), .number(7), .divide,
+      .number(6), .number(5), .number(4), .multiply,
+      .number(3), .number(2), .number(1), .subtract,
+      .number(0), .addDecimal, .delete, .add
     ]
+
+    return actions.map { CalculatorButton(withAction: $0) }
   }
 
 }

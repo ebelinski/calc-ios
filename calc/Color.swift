2e090d460f07488437a9e4f49e3a89d67a28f414
Eugene Belinski <ebelinski@protonmail.com>
2018-03-25 19:22:19 -0500

    created appBackground color

--- calc/Color.swift
+++ calc/Color.swift
@@ -1,5 +1,6 @@
 import UIKit
 
 enum Color {
+  static let appBackground = UIColor(red: 0.22, green: 0.341, blue: 0.376, alpha: 1) // #385760
   static let grayBackground = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1)
 }
